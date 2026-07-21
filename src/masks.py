e28da55d3e1de5add74afb1b606657ebb5706b92
alena <alenasinging@gmail.com>
2026-07-21 20:40:59 +0300

    init project

---
+++ src/masks.py
@@ -0,0 +1,14 @@
+def get_mask_card_number(card_number: str) -> str:
+    """Маскирует номер банковской карты в формат XXXX XX** **** XXXX"""
+    # Нужно поделить на части полученное значение
+    part1 = card_number[:4]
+    part2 = card_number[4:6] + "**"
+    part3 = "****"
+    part4 = card_number[-4:]
+
+    # Из этих пунктов теперь собираем строку
+    return f"{part1} {part2} {part3} {part4}"
+
+
+def get_mask_account(card_number: str) -> str:
+    return f"**{card_number[-4:]}"
